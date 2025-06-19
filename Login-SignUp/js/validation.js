11d5e4e31d67ea780c5febbc04c6f6e893310d3a
takedajouji <takedajouji@gmail.com>
2025-06-19 00:54:17 -0500

    Login - Signup page project (vanilla html, css, and js)

--- Login-SignUp/js/validation.js
+++ Login-SignUp/js/validation.js
@@ -3,6 +3,7 @@ const firstname_input = document.getElementById('firstname-input')
 const email_input = document.getElementById('email-input')
 const password_input = document.getElementById('password-input')
 const repeat_password_input = document.getElementById('repeat-password-input')
+const error_message=document.getElementById("error-message")
 
 form.addEventListener('submit', (e) =>{
    // e.preventDefault()
@@ -18,6 +19,7 @@ form.addEventListener('submit', (e) =>{
     }
     if(errors.length > 0){
         e.preventDefault()
+        error_message.innerText = errors.join(". ")
     }
 })
 
@@ -38,5 +40,40 @@ function getSignupFormErrors(firstname, email, password, repeatPassword){
         password_input.parentElement.classList.add('incorrect')
     }
 
+    if(password.length < 8){
+        errors.push('Password is required')
+        password_input.parentElement.classList.add('incorrect')
+    }
+    if(password ==='' || repeatPassword == null){
+        errors.push('Password does not match')
+        password_input.parentElement.classList.add('incorrect')
+        password_input.parentElement.classList.add('incorrect')
+        repeat_password_input.parentElement.classList.add('incorrect')
+    }
+
     return errors;
 }
+
+function getLoginFormsErrors(email, password){
+    let errors = []
+    if(email === '' || email == null){
+        errors.push('Email is required')
+        email_input.parentElement.classList.add('incorrect')
+    }
+    if(password ==='' || password == null){
+        errors.push('Password is required')
+        password_input.parentElement.classList.add('incorrect')
+    }
+    return errors;
+}
+
+const allInputs = [firstname_input, password_input, email_input, repeat_password_input]
+
+allInputs.forEach(input => {
+    input.addEventListener('input', () =>{
+        if(input.parentElement.classList.contains('incorrect')){
+            input.parentElement.classList.remove('incorrect')
+            error_message.innerText = ''
+        }
+    })
+})
